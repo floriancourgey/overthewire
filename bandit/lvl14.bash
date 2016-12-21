ee635519777ab67d729c829279b4f5c8426e74fc
Florian Courgey <florian@floriancourgey.com>
2016-12-21 16:07:58 +0100

    + bandit 13, 14, 15

---
+++ bandit/lvl14.bash
@@ -0,0 +1,2 @@
+cat /etc/bandit_pass/bandit14 | nc localhost 30000
+BfMYroe26WYalil77FoDi9qh59eK5xNr
---
+++ bandit/lvl15.bash
@@ -0,0 +1,3 @@
+openssl s_client -ign_eof  -connect localhost:30001
+# une fois à l'interérieur, on envoie le précédent pass, qui nous donne :
+cluFn7wTiGryunymYOu4RcffSxQluehd
